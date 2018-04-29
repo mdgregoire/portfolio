90e23c0e69a98ff575ce57c688ebbc934b078cc2
Michael Gregoire <mdgregoire@gmail.com>
2018-04-29 16:49:17 -0500

    cleanup

--- server/public/scripts/services/HomeService.js
+++ server/public/scripts/services/HomeService.js
@@ -2,10 +2,6 @@ myApp.service('HomeService', ['$http', '$location', function($http, $location){
   console.log('in homeService');
   let self = this;
 
-  self.submitText = function(text){
-    console.log('in submit text', text);
-  }
-
   self.openMenu = function($mdMenu, ev) {
     originatorEv = ev;
     $mdMenu.open(ev);
